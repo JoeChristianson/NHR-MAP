5b9cd569517dfd0e254ba6863372a8172619f658
Joe Christianson <jcfargond@gmail.com>
2022-04-12 10:23:53 -0500

    made data writer

---
+++ modules/dataWriter.js
@@ -0,0 +1,16 @@
+const {readFile,writeFile} = require('fs')
+const util = require("util")
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile)
+
+const write = async (county,state,places)=>{
+    const data = await readFilePromise(`../data/${state}.json`,"utf-8")
+    const obj = JSON.parse(data);
+    obj[county] = places;
+    const str = JSON.stringify(obj)
+    writeFilePromise(`../data/${state}.json`,str)
+
+}
+
+write("Cass","North_Dakota",{t:"t"})
+
---
+++ modules/scraper.js
@@ -0,0 +1,42 @@
+const puppeteer = require('puppeteer')
+const dataWriter = require("./dataWriter")
+
+async function scrape(county,state){
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox','--disable-setuid-sandbox']
+      });
+    const url = `https://en.wikipedia.org/wiki/National_Register_of_Historic_Places_listings_in_${county}_County,_${state}`
+    console.log(url)
+    const page = await browser.newPage();
+    await page.goto(url);
+    const headings = await page.evaluate(()=> Array.from(document.querySelectorAll('.sortable td .mapframe-coord-name a'),element=> [element.textContent,element.href]));
+    console.log(headings[0])
+    const images = await page.evaluate(()=> Array.from(document.querySelectorAll('tbody img'),element=> element.src,));
+    console.log(images[0])
+    const addresses = await page.evaluate(()=> Array.from(document.querySelectorAll('.adr span.label'),element=> element.textContent));
+    console.log(addresses[0]);
+    const latitudes = await page.evaluate(()=> Array.from(document.querySelectorAll('.latitude'),element=> element.textContent));
+    const longitudes = await page.evaluate(()=> Array.from(document.querySelectorAll('.longitude'),element=> element.textContent));
+    console.log(latitudes[0])
+    console.log(longitudes[0])
+    const localities = await page.evaluate(()=> Array.from(document.querySelectorAll('.locality'),element=> element.textContent));
+    console.log(localities[0])
+    browser.close()
+    const places = [];
+    for(let i=0;i<headings.length;i++){
+        const place = {
+            name: headings[i][0],
+            link: headings[i][1],
+            image:images[i],
+            latitude:latitudes[i],
+            longitude:longitudes[i],
+            locality:localities[i],
+        }
+        places.push(place)
+    }
+    
+}
+
+scrape(process.argv[2],process.argv[3])
+
