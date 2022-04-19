3394691547a2653e3316079cc115337aae59c9b4
Joe Christianson <jcfargond@gmail.com>
2022-04-18 19:22:11 -0500

    added api path

--- client/assets/scripts/nhrCall.js
+++ client/assets/scripts/nhrCall.js
@@ -1,6 +1,6 @@
 const getData = async (county,state)=>{
     console.log(state);
-    const url = `http://localhost:4000/api?county=${county}&state=${state}`
+    const url = `/api?county=${county}&state=${state}`
     const res = await fetch(url);
     console.log(res);
     const data = await res.json();
